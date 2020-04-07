4d7aea72467e2e48187be28463a2340dbd0b227f
antonio490 <asc490@hotmail.com>
2020-04-07 21:45:29 +0200

    update cam columns csv

--- cam.py
+++ cam.py
@@ -3,9 +3,7 @@ import time
 import sys
 import cv2
 import csv
-import cpbd
 from csv import writer
-from scipy import ndimage
 import picamera
 import numpy as np
 from time import ctime, sleep
@@ -15,10 +13,11 @@ from datetime import datetime
 MIN_STEP = 0
 MAX_STEP = 10
 MIN_FOCUS = 0
+IMG_SIZE = 244
 
 ls_LAPV = []
 ls_TENG = []
-ls_CPBD = []
+ls_LAPM = []
 images_path = []
 
 def TENG(img):
@@ -33,13 +32,17 @@ def TENG(img):
     return np.mean(gaussianX * gaussianX +
                       gaussianY * gaussianY)
 
-def MLOG(img):
-    """Implements the MLOG focus measure algorithm.
+def LAPM(img):
+    """Implements the Modified Laplacian (LAP2) focus measure
+    operator. Measures the amount of edges present in the image.
     :param img: the image the measure is applied to
     :type img: numpy.ndarray
     :returns: numpy.float32 -- the degree of focus
     """
-    return np.max(cv2.convertScaleAbs(cv2.Laplacian(img, 3)))
+    kernel = np.array([-1, 2, -1])
+    laplacianX = np.abs(cv2.filter2D(img, -1, kernel))
+    laplacianY = np.abs(cv2.filter2D(img, -1, kernel.T))
+    return np.mean(laplacianX + laplacianY)
 
 def LAPV(img):
     """Implements the Variance of Laplacian (LAP4) focus measure
@@ -81,13 +84,21 @@ def stepDue(localMax, i):
     
     return due
 
-def trend(i):
+def tendency(i):
     if ls_LAPV[i-1] <= ls_LAPV[i-2]:
         trend = 'down'
     else:
         trend = 'up'
 
     return trend
+
+def ratio(i):
+    if i==0:
+        ratio = 0 
+    else:
+        ratio = ls_LAPV[i] / ls_LAPV[i-1]
+
+    return ratio
 
 def getConfCam(picam):
     print("Shutter speed: ", picam.shutter_speed)
@@ -112,67 +123,47 @@ def setConfCam(picam):
     picam.hflip = True
     print("CONFIGURATION SET CORRECTLY")
 
-'''
-def insertRowCSV(step, LAPV, MLOG, TENG, path):
-    
-    csv_file = "album.csv"
-    list_of_elem = [step, LAPV, MLOG, TENG, path]
-
-
-    if os.path.exists(csv_file):
-        print("insertRow")
-        with open(csv_file, 'a+', newline='') as write_obj:
-            csv_writer = writer(write_obj)
-            csv_writer.writerow(list_of_elem)
-
-    else:
-        headers = ['STEP','LAPV','MLOG','TENG', 'IMG_PATH']
-        with open(csv_file, 'w', newline='') as write_obj:
-            csv_writer = writer(write_obj)
-            csv_writer.writerow(headers)
-            csv_writer.writerow(list_of_elem)
-
-'''
 
 def updateCSV(localMaxPos):
     
     csv_file = "album.csv"
-    due = 'none'
-    ratio = 0
 
     if os.path.exists(csv_file):
         with open(csv_file, 'a+', newline='') as write_obj:
             csv_writer = writer(write_obj)
-            for i in range(1,11):
+            for i in range(MIN_STEP, MAX_STEP):
                 due = stepDue(localMaxPos, i)
+                trend = tendency(i)
+                r = ratio(i)
+                step = i+1
 
-                if i==1:
-                    fm = ls_LAPV[i-1]
-                    ls_LAPV.insert(0, fm)
-
-                ratio = ls_LAPV[i-1] / ls_LAPV[i-2]
-                trend = trend(i)
-
-
-                list_of_elem = [i, ls_LAPV[i-1], ls_TENG[i-1], ls_CPBD[i-1], ls_LAPV[i-2], ls_LAPV[i], (localMaxPos-i), ratio, trend, images_path[i-1], due]
+                if i==MIN_STEP:
+                    list_of_elem = [step, ls_LAPV[i], ls_TENG[i], ls_LAPM[i], ls_LAPV[i], ls_LAPV[i+1], (localMaxPos-i), r, trend, images_path[i], due]
+                elif i==(MAX_STEP-1):
+                    list_of_elem = [step, ls_LAPV[i], ls_TENG[i], ls_LAPM[i], ls_LAPV[i-1], ls_LAPV[i], (localMaxPos-i), r, trend, images_path[i], due]
+                else:
+                    list_of_elem = [step, ls_LAPV[i], ls_TENG[i], ls_LAPM[i], ls_LAPV[i-1], ls_LAPV[i+1], (localMaxPos-i), r, trend, images_path[i], due]
+                
                 csv_writer.writerow(list_of_elem)
 
     else:
-        headers = ['STEP','LAPV','TENG', 'CPBD', 'prevF', 'nextF', 'MV_STEP', 'ratio', 'trend','IMG_PATH', 'due']
+        headers = ['STEP','LAPV','TENG', 'LAPM', 'prevF', 'nextF', 'MV_STEP', 'ratio', 'trend','IMG_PATH', 'due']
         with open(csv_file, 'w', newline='') as write_obj:
             csv_writer = writer(write_obj)
             csv_writer.writerow(headers)
-            for i in range(1,11):
+            for i in range(MIN_STEP, MAX_STEP):
                 due = stepDue(localMaxPos, i)
+                trend = tendency(i)
+                r = ratio(i)
+                step = i+1
 
-                if i==1:
-                    fm = ls_LAPV[i-1]
-                    ls_LAPV.insert(0,fm)
-                
-                ratio = ls_LAPV[i-1] / ls_LAPV[i-2]
-                trend = trend(i)
+                if i==MIN_STEP:
+                    list_of_elem = [step, ls_LAPV[i], ls_TENG[i], ls_LAPM[i], ls_LAPV[i], ls_LAPV[i+1], (localMaxPos-i), r, trend, images_path[i], due]
+                elif i==(MAX_STEP-1):
+                    list_of_elem = [step, ls_LAPV[i], ls_TENG[i], ls_LAPM[i], ls_LAPV[i-1], ls_LAPV[i], (localMaxPos-i), r, trend, images_path[i], due]
+                else:
+                    list_of_elem = [step, ls_LAPV[i], ls_TENG[i], ls_LAPM[i], ls_LAPV[i-1], ls_LAPV[i+1], (localMaxPos-i), r, trend, images_path[i], due]
 
-                list_of_elem = [i, ls_LAPV[i-1], ls_TENG[i-1], ls_CPBD[i-1], ls_LAPV[i-2], ls_LAPV[i], (localMaxPos-i), ratio, trend, images_path[i-1], due]
                 csv_writer.writerow(list_of_elem)
 
 
@@ -187,16 +178,19 @@ def calcFocus(path):
     img = cv2.imread(path)
 
     fmLPAV = LAPV(img)
-    fmMLOG = MLOG(img)
     fmTENG = TENG(img)
+    fmLAPM = LAPM(img)
+    
+    fmLPAV = round(fmLPAV, 5)
+    fmLAPM = round(fmLAPM, 5)
+    fmTENG = round(fmTENG, 5)
 
-    fmCPBD = cpbd.compute(img)
 
     ls_LAPV.append(fmLPAV)
     ls_TENG.append(fmTENG)
-    ls_CPBD.append(fmCPBD)
+    ls_LAPM.append(fmLAPM)
 
-    return fmLPAV, fmMLOG, fmTENG
+    return fmLPAV, fmLAPM, fmTENG
 
 
 def takePhoto(picam, i, step, path):
@@ -208,18 +202,14 @@ def takePhoto(picam, i, step, path):
     timestamp = int(datetime.timestamp(now))
 
     name = "IMG_"+str(timestamp)+".png"
-    picam.capture(name,resize=(244,244))
+    picam.capture(name,resize=(IMG_SIZE,IMG_SIZE))
 
-    #LAPV, MLOG, TENG = calcFocus(name) 
     calcFocus(name)
     path = path+"/"+name
-
     images_path.append(path)
 
-    #insertRowCSV(step, LAPV, MLOG, TENG, path) 
-
     picam.stop_preview()
-    print("PHOTO TAKEN")
+    print("PHOTO %d DONE" % i)
 
 if __name__ == "__main__":
 
@@ -247,9 +237,8 @@ if __name__ == "__main__":
             takePhoto(picam, i, sf, path)
             stepFocus(sf)
         
-        maxLPAV = max(ls_LAPV)
-        indexMax = ls_LAPV.index(max(ls_LAPV)) + 1
-        print("MAX: %d POSITION: %d" % (maxLPAV, indexMax))
+        indexMax = ls_LAPV.index(max(ls_LAPV))
+        print("MAX: %d POSITION: %d" % (ls_LAPV[indexMax], indexMax+1))
 
         updateCSV(indexMax)
 
