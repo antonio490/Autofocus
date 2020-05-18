3f6b87cc905755f013026f50193077a45476946a
antonio490 <asc490@hotmail.com>
2020-05-18 12:38:20 +0200

    dir names - orderFolders

--- orderFolders.py
+++ orderFolders.py
@@ -8,6 +8,8 @@ from csv import writer
 
 dSmall = "img_root/small"
 dBig = "img_root/big"
+dFocus = "img_root/focus"
+
 
 fAlbum = "album/"
 
@@ -15,7 +17,7 @@ fAlbum = "album/"
 def createFolders():
 
     wdir = os.getcwd()
-    new_dirs = ["/img_root", "/small", "/big"]
+    new_dirs = ["/img_root", "/small", "/big", "/focus"]
 
     if not os.path.exists(wdir+new_dirs[0]):
         os.mkdir(wdir+new_dirs[0]) 
@@ -25,6 +27,9 @@ def createFolders():
 
     if not os.path.exists(wdir+new_dirs[0]+new_dirs[2]):
         os.mkdir(wdir+new_dirs[0]+new_dirs[2]) 
+
+    if not os.path.exists(wdir+new_dirs[0]+new_dirs[3]):
+        os.mkdir(wdir+new_dirs[0]+new_dirs[3])
 
 
 '''
@@ -37,21 +42,27 @@ we store orderly images.
 '''
 def orderImages():
 
-    col_names = ['STEP','LAPV','TENG','LAPM','prevF','nextF', 'ratio','trend','IMG_PATH','due']
+    col_names = ['STEP','LAPV','TENG','LAPM','prevF','nextF', 'MV_STEP', 'ratio','trend','IMG_PATH','due']
 
     album = pd.read_csv('~/Desktop/Autofocus/album/album.csv', header=0, names=col_names)
 
     # loop over every row and read IMG_PATH and due columns
     for row in album.itertuples():
         due = row.due
+        mv_step = row.MV_STEP
         img_path = row.IMG_PATH[-18:]
 
         print("IMG_PATH: %s" % img_path)
 
-        if due == "small":
-            shutil.copy(fAlbum + img_path, dSmall)
-        elif due == "big":
-            shutil.copy(fAlbum + img_path, dBig)
+
+        if mv_step == 0:
+            shutil.copy(fAlbum + img_path, dFocus)
+
+        elif mv_step != 0:
+            if due == 'small':
+                shutil.copy(fAlbum + img_path, dSmall)
+            else:
+                shutil.copy(fAlbum + img_path, dBig)
         else:
             print("Do nothing")
 
